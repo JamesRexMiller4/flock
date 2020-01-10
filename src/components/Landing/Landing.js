f9cd7a70100adfee0c2dcbd23f594a7635e75acc
James Miller <james.rex.miller.4@gmail.com>
2020-01-10 00:42:17 -0700

    Finish styles and html for Landing component

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -2,7 +2,8 @@ import React, {Component} from 'react';
 import './Landing.scss';
 import search from '../../images/search.svg';
 import group from '../../images/user-friends-solid.svg';
-import message from '../../images/comment.png'
+import message from '../../images/comment.png';
+import twitter from '../../images/goldtwitter.png';
 
 class Landing extends Component {
   constructor(props) {
@@ -22,13 +23,20 @@ class Landing extends Component {
             <p className='landing-p'><img src={message} alt='text-bubble' className='icon'/>Join the conversation.</p>
           </div>
         </div>
-        <div>
+        <div className='right-landing-div'>
           <form className='form'>
-            <input placeHolder='Phone, email, username' value={username}></input>
-            <input placeHolder='Password' value={password}></input>
-            <button className='btn'>Log In</button>
+            <input placeHolder='Phone, email, or username' value={this.state.username} 
+            name='username' className='input-form' autoComplete='off'></input>
+            <input placeHolder='Password' value={this.state.password} 
+            name='password' className='input-form' autoComplete='off'></input>
+            <button type='button' className='login btn'>Log in</button>
           </form>
-          <h1>Welcome to Twitter!</h1>
+          <div className='welcome-message-div'>
+            <img src={twitter} alt='twitter-icon' className='twitter-icon'/>
+            <h2>See what's happening in <br></br> the world right now</h2>
+            <h3 className="h3-welcome">Join Twitter today.</h3>
+            <button type='button' className='sign-up btn'>Sign up</button>
+          </div>
         </div>
       </section>
     );
