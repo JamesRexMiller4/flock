d3efe0972b406a578e39c3faa73b99c142e17272
James Miller <james.rex.miller.4@gmail.com>
2020-01-09 23:37:21 -0700

    Add styles to Landing component and build out more elements within it

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,14 +1,35 @@
 import React, {Component} from 'react';
+import './Landing.scss';
+import search from '../../images/search.svg';
+import group from '../../images/user-friends-solid.svg';
+import message from '../../images/comment.png'
 
 class Landing extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      username: '',
+      password: ''
+    }
   }
   render() { 
     return ( 
-      <section>
-        <h1>Welcome to Twitter!</h1>
+      <section className='landing-section-wrapper'>
+        <div className='background-image-div-landing'>
+          <div className='paragraph-div-container'>
+            <p className='landing-p'><img src={search} alt='magnifying-glass' className='icon'/> Follow your interests.</p>
+            <p className='landing-p'><img src={group} alt='group' className='icon'/> Filter only the accounts you wish to see.</p>
+            <p className='landing-p'><img src={message} alt='text-bubble' className='icon'/>Join the conversation.</p>
+          </div>
+        </div>
+        <div>
+          <form className='form'>
+            <input placeHolder='Phone, email, username' value={username}></input>
+            <input placeHolder='Password' value={password}></input>
+            <button className='btn'>Log In</button>
+          </form>
+          <h1>Welcome to Twitter!</h1>
+        </div>
       </section>
     );
   }
