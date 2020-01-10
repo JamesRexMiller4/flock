e45eba0690fdb5db26f954a7d6e80437b5935485
James Miller <james.rex.miller.4@gmail.com>
2020-01-09 20:56:40 -0700

    Import NavBar and Timeline component/container into Home component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,2 +1,9 @@
 import React from 'react';
 import NavBar from '../NavBar/NavBar';
+import TimeLine from '../../containers/TimeLine'
+
+const Home = () => {
+  return (  );
+}
+ 
+export default Home;
